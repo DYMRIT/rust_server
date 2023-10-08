a50dcd0632f8ad7cc7cfc8758d882cd326891efe
DYMRIT <homadmitry2001@gmail.com>
2023-10-08 18:55:46 +0300

    third commit

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ fn main() {
     println!("HTTP/1.1 200 OK\r");
     println!("Content-Type: text/plain\r\n");
     println!("Hello world!");
-    let l = env::args_os();
-    for argument in l {
-        println!("{argument:?}");
-    }
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
