78cfbac501467fdf38f3a48c9804bea25f7b7155
DYMRIT <homadmitry2001@gmail.com>
2023-10-08 18:27:50 +0300

    second commit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
     println!("HTTP/1.1 200 OK\r");
     println!("Content-Type: text/plain\r\n");
-    println!("Hello world");
+    println!("Hello world!");
     let l = env::args_os();
     for argument in l {
         println!("{argument:?}");
