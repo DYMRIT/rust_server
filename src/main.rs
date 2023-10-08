33aacaca81ce650fb1830452c2c3361c36e5f37e
DYMRIT <homadmitry2001@gmail.com>
2023-10-08 17:52:58 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::env;
+
+fn main() {
+
+    println!("HTTP/1.1 200 OK\r");
+    println!("Content-Type: text/plain\r\n");
+    println!("Hello world");
+    let l = env::args_os();
+    for argument in l {
+        println!("{argument:?}");
+    }
+}
