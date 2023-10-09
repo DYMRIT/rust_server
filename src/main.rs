a59da6f6c8fe79718b4d1e05ba091b58d63ea0a8
DYMRIT <homadmitry2001@gmail.com>
2023-10-09 10:27:58 +0300

    work with get request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::env;
+use std::process::Command;
 
 fn main() {
 
     println!("HTTP/1.1 200 OK\r");
     println!("Content-Type: text/plain\r\n");
     println!("Hello world!");
-    let args: Vec<String> = env::args().collect();
-    let query_string = args.get(1).unwrap_or(&String::new());
-    println!("{:?}", query_string);
+
+    let result = Command::new("curl")
+        .arg("https://jsonplaceholder.typicode.com/posts/2")
+        .output()
+        .expect("failed to execute command");
+    println!("status: {}", result.status);
+    println!("stdout: {}", String::from_utf8_lossy(&result.stdout));
 }
